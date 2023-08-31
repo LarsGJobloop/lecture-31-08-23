c7f80b63eba6328fb535c08d880f75ee6b615335
LarsG <lars.gunnar@jobloop.no>
2023-08-31 08:39:35 +0000

    add examples for static assets

--- static-assets/src/App.jsx
+++ static-assets/src/App.jsx
@@ -2,6 +2,7 @@ import { ProjectCard } from "./components/ProjectCard/ProjectCard"
 import { LayoutBase } from "./layouts/LayoutBase/LayoutBase"
 
 import './index.css'
+import projectImage from './assets/project-1-image.avif'
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         
         <ProjectCard
           name="First HTML project"
-          imageUrl="https://plus.unsplash.com/premium_photo-1680104511613-ac897df8c6a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          imageUrl={projectImage}
           description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. In sunt obcaecati inventore quos dolorem dolore facilis ex, totam deserunt ea, earum, distinctio et est. Rem veniam nobis provident cupiditate ea?"
           githubLink="https://github.com"
         />
