32f29692757f70ae561d35597de8949a87e8f6f3
LarsG <lars.gunnar@jobloop.no>
2023-08-31 09:05:10 +0000

    add rendering lists of data

--- static-assets/src/App.jsx
+++ static-assets/src/App.jsx
@@ -1,30 +1,33 @@
 import { ProjectCard } from "./components/ProjectCard/ProjectCard"
 import { LayoutBase } from "./layouts/LayoutBase/LayoutBase"
 
+import { projects } from "./data/projects"
 import './index.css'
-import projectImage from './assets/project-1-image.avif'
 
 function App() {
+
   return (
     <LayoutBase>
       <section>
         <h2>Component properties</h2>
         
-        <ProjectCard
-          name="First HTML project"
-          imageUrl={projectImage}
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. In sunt obcaecati inventore quos dolorem dolore facilis ex, totam deserunt ea, earum, distinctio et est. Rem veniam nobis provident cupiditate ea?"
-          githubLink="https://github.com"
-        />
-
-        <ProjectCard
-          name="Second HTML and CSS project"
-          imageUrl="https://plus.unsplash.com/premium_photo-1680104511613-ac897df8c6a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. In sunt obcaecati inventore quos dolorem dolore facilis ex, totam deserunt ea, earum, distinctio et est. Rem veniam nobis provident cupiditate ea?"
-          githubLink="https://github.com"
-        />
-
-        <ProjectCard  />
+        <ul>
+          {
+            projects.map((project) => {
+                return (
+                  <li key={project.id}>
+                    <ProjectCard
+                      name={project.name}
+                      description={project.description}
+                      githubLink={project.githubLink}
+                      imageUrl={project.imageUrl}
+                    />
+                  </li>
+                )
+              }
+            )
+          }
+        </ul>
 
       </section>
     </LayoutBase>
---
+++ static-assets/src/data/projects.js
@@ -0,0 +1,25 @@
+import projectImage from '../assets/project-1-image.avif'
+
+export const projects = [
+  {
+    id: 1,
+    name: "First HTML project",
+    imageUrl: projectImage,
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In sunt obcaecati inventore quos dolorem dolore facilis ex, totam deserunt ea, earum, distinctio et est. Rem veniam nobis provident cupiditate ea?",
+    githubLink: "https://github.com",
+  },
+  {
+    id: 2,
+    name: "First HTML project",
+    imageUrl: projectImage,
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In sunt obcaecati inventore quos dolorem dolore facilis ex, totam deserunt ea, earum, distinctio et est. Rem veniam nobis provident cupiditate ea?",
+    githubLink: "https://github.com",
+  },
+  {
+    id: 3,
+    name: "Generating components from data",
+    imageUrl: projectImage,
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In sunt obcaecati inventore quos dolorem dolore facilis ex, totam deserunt ea, earum, distinctio et est. Rem veniam nobis provident cupiditate ea?",
+    githubLink: "https://github.com",
+  }
+]
